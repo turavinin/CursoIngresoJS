8edabd974a2af4eed8ad11e8b97af38442b90ccb
turavinin <aturavinin@gmail.com>
2021-01-29 18:58:37 -0300

    Finalizado el ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-  alert('Hola a todos');
+  alert('Esto funciona de maravilla');
 }
