79f9c6c4cbc8f35f0ef2ff42762b51ed7421dc36
turavinin <aturavinin@gmail.com>
2021-02-04 19:01:13 -0300

    finalizado tema 3, ejerc. 7

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,18 @@
-function mostrar()
-{
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+function mostrar() {
+  let lugar = document.getElementById('txtIdDestino').value;
 
-}//FIN DE LA FUNCIÓN
+  switch (lugar) {
+    case 'Bariloche':
+      alert('Oeste');
+      break;
+    case 'Cataratas':
+      alert('Norte');
+      break;
+    case 'Mar del plata':
+      alert('Este');
+      break;
+    case 'Ushuaia':
+      alert('Sur');
+      break;
+  }
+} //FIN DE LA FUNCIÓN
