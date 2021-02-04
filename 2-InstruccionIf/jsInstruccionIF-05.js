15ec565872234dca6bfbc5998d8c44918dc011c4
turavinin <aturavinin@gmail.com>
2021-02-04 17:33:36 -0300

    finalizado el tema 2, ejercicio 5

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,8 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+function mostrar() {
+  //tomo la edad
+  let edad = parseInt(document.getElementById('txtIdEdad').value);
 
-}//FIN DE LA FUNCIÓN
+  if (edad < 13 || edad > 17) {
+    alert('La persona no es adolescente');
+  }
+} //FIN DE LA FUNCIÓN
