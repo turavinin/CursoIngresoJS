900fa7120119053697836d49d223c97a7cdbec05
turavinin <aturavinin@gmail.com>
2021-02-04 18:55:03 -0300

    finalizado el tema 3, ejerc. 6

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -1,7 +1,18 @@
-function mostrar()
-{
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+function mostrar() {
+  let hora = Math.floor(parseFloat(document.getElementById('txtIdHora').value));
 
-}//FIN DE LA FUNCIÓN
+  switch (true) {
+    case hora >= 7 && hora <= 11:
+      alert('Es de mañana');
+      break;
+    case hora >= 12 && hora <= 19:
+      alert('Es de tarde');
+      break;
+    case (hora >= 20 && hora <= 24) || (hora >= 00 && hora <= 6):
+      alert('Es de noche');
+      break;
+    default:
+      alert(`Esa hora no existe`);
+      break;
+  }
+} //FIN DE LA FUNCIÓN
