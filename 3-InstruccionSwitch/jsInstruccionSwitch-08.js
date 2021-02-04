e11f8ddbb357d8d090dfe80539fef8d051fb1465
turavinin <aturavinin@gmail.com>
2021-02-04 19:02:36 -0300

    finalizado tema 3, ejerc. 8

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,18 @@
-function mostrar()
-{
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+function mostrar() {
+  let lugar = document.getElementById('txtIdDestino').value;
 
-}//FIN DE LA FUNCIÓN
+  switch (lugar) {
+    case 'Bariloche':
+      alert('Frio');
+      break;
+    case 'Cataratas':
+      alert('Calor');
+      break;
+    case 'Mar del plata':
+      alert('Calor');
+      break;
+    case 'Ushuaia':
+      alert('Frio');
+      break;
+  }
+} //FIN DE LA FUNCIÓN
