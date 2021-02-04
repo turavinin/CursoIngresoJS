440d28253a28612c98919f02e456456c68818663
turavinin <aturavinin@gmail.com>
2021-02-04 17:46:03 -0300

    Finalizado el tema 2, ejercicio 8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,8 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
-	
+function mostrar() {
+  let edad = parseInt(document.getElementById('txtIdEdad').value);
+  let estadoCivil = document.getElementById('estadoCivil').value;
 
-
-}//FIN DE LA FUNCIÓN
+  if (edad >= 18 && estadoCivil == 'Soltero') {
+    alert('Es soltero y no es menor.');
+  }
+} //FIN DE LA FUNCIÓN
