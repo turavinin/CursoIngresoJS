ba70c3c556f19be88bcf37d2cb7696b3be178f3a
turavinin <aturavinin@gmail.com>
2021-02-04 18:47:54 -0300

    finalizado el tema 3, ejerc. 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,16 @@
-function mostrar()
-{
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
+function mostrar() {
+  let hora = parseFloat(document.getElementById('txtIdHora').value);
 
-
-
-}//FIN DE LA FUNCIÓN
+  switch (Math.floor(hora)) {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      alert(`Es de mañana.`);
+      break;
+    default:
+      alert(`NO es de mañana`);
+      break;
+  }
+} //FIN DE LA FUNCIÓN
