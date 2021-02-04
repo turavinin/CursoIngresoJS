e93d2e680eba36892a52165c91b0569b15b53d34
turavinin <aturavinin@gmail.com>
2021-02-04 17:58:31 -0300

    finalizado tema 2, ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,8 @@
-function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+function mostrar() {
+  // Math random retorna un numero entre 0 y 1 => Luego se multiplica por 10 que es el rango
+  // => Math floor luego redondea el numero hacia abajo (puede quedar un 6.38 antes) =>
+  // => el + 1 sirve para que no de 0 y que de 10.
+  let numeroRandom = Math.floor(Math.random() * 10) + 1;
 
-}//FIN DE LA FUNCIÓN
+  alert(numeroRandom);
+} //FIN DE LA FUNCIÓN
