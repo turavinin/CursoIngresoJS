ddf5d3609632e7371638645478e482eb4da10bcf
turavinin <aturavinin@gmail.com>
2021-02-04 17:29:10 -0300

    finalizar el ejercicio tema 2, ej. 3

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,9 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+function mostrar() {
+  let edad = parseInt(document.getElementById('txtIdEdad').value);
 
-}//FIN DE LA FUNCIÓN
+  if (edad > 18) {
+    alert('La persona es mayor de edad');
+  } else {
+    alert('La persona es menor de edad');
+  }
+} //FIN DE LA FUNCIÓN
