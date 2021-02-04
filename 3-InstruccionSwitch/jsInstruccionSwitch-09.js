c83a64348a8b6a95370a6822d87bb742d2e1fd24
turavinin <aturavinin@gmail.com>
2021-02-04 20:06:56 -0300

    finalizado tema 3, ejerc 9

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,89 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+  // Almacenar estacion en variable
+  let estacion = document.getElementById('txtIdEstacion').value;
+  // Almacenar destino en variable
+  let destino = document.getElementById('txtIdDestino').value;
+  // Almacenar en variable precio de estadía base
+  let precioBase = 15000;
+  // Almacenar precio base + 20%
+  let precioMas20 = precioBase + precioBase * 0.2;
+  // Almacenar precio base - 20&
+  let precioMenos20 = precioBase - precioBase * 0.2;
+  // Almacenar precio base + 10%
+  let precioMas10 = precioBase + precioBase * 0.1;
+  // Almacenar precio base - 10%
+  let precioMenos10 = precioBase - precioBase * 0.1;
 
-}//FIN DE LA FUNCIÓN
+  // Metodo SWITCH
+  // Dentro otro SWITHC
+
+  switch (estacion) {
+    case 'Invierno':
+      {
+        switch (destino) {
+          case 'Bariloche':
+            alert(
+              `Destino de ${destino} en ${estacion}, el precio es \$${precioMas20}`
+            );
+            break;
+
+          case 'Mar del plata':
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioMenos20}`
+            );
+            break;
+
+          default:
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioMenos10}`
+            );
+            break;
+        }
+      }
+      break;
+
+    case 'Verano':
+      {
+        switch (destino) {
+          case 'Bariloche':
+            alert(
+              `Destino de ${destino} en ${estacion}, el precio es \$${precioMenos20}`
+            );
+            break;
+
+          case 'Mar del plata':
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioMas20}`
+            );
+            break;
+
+          default:
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioMas10}`
+            );
+            break;
+        }
+      }
+      break;
+
+    case 'Otoño':
+    case 'Primavera':
+      {
+        switch (destino) {
+          case 'Cordoba':
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioBase}`
+            );
+            break;
+
+          default:
+            alert(
+              `Para el destino de ${destino} en ${estacion}, el precio es \$${precioMas10}`
+            );
+            break;
+        }
+      }
+      break;
+  }
+}
+//FIN DE LA FUNCIÓN
