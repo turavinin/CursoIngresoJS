956bfc99c19186ab54967d19f9ac59d95b17cc79
turavinin <aturavinin@gmail.com>
2021-02-04 18:20:39 -0300

    finalizado el tema 3, ejerc. 1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,22 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+function mostrar() {
+  //tomo el mes
+  let mes = document.getElementById('txtIdMes').value;
 
-
-
-}//FIN DE LA FUNCIÓN
+  switch (mes) {
+    case 'Enero':
+      alert('Qué comiences bien el año!!');
+      break;
+    case 'Marzo':
+      alert('A clases!!');
+      break;
+    case 'Julio':
+      alert('Se vienen las vacaciones!!');
+      break;
+    case 'Diciembre':
+      alert('Felices fiestas!!');
+      break;
+    default:
+      alert('Mes comun');
+      break;
+  }
+} //FIN DE LA FUNCIÓN
