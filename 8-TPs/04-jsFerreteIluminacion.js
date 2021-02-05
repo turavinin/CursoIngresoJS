0cde70bbefde403ecae3c24921407dfbd56b040d
turavinin <aturavinin@gmail.com>
2021-02-05 13:58:52 -0300

    finalizado el TP 4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,75 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+  // Almacenar en variable integer de cantidad de lamparitas
+  let cantidadLampartias = parseInt(document.getElementById("txtIdCantidad").value);
+  // Almacenar en variable la eleccion de empresa de lampartias
+  let empresa = document.getElementById('Marca').value;
+  // Almacenar en variable el campo value de precio con descuento
+  let campoPrecioDescuento = document.getElementById("txtIdprecioDescuento");
+  // Almacenar en variable el precio base de lamparita
+  let precioBase = 35 * cantidadLampartias;
+  // Declarar variable precio con descuento
+  let precioConDescuento;
+  // Declarar varialbe desde impuestos
+  let impuestos; 
+
+  // If => cantidad de lampartias > 6 => total - 50%
+  if (cantidadLampartias >= 6) {
+    precioConDescuento = precioBase - precioBase * 0.5;
+  }
+
+  // Switch (key => cantidad lampartias)
+  switch (cantidadLampartias) {
+    case 5:
+      switch (empresa) {
+        case "ArgentinaLuz":
+          precioConDescuento = precioBase - precioBase * 0.4;
+          break;
+        default:
+          precioConDescuento = precioBase - precioBase * 0.3;
+          break;
+      }
+      break;
+
+    case 4:
+      switch (empresa) {
+        case "ArgentinaLuz":
+        case "FelipeLamparas":
+          precioConDescuento = precioBase - precioBase * 0.25;
+          break;
+        default:
+          precioConDescuento = precioBase - precioBase * 0.20;
+          break;
+      }
+      break;
+
+    case 3:
+        switch (empresa) {
+          case 'ArgentinaLuz':
+            precioConDescuento = precioBase - precioBase * 0.15;
+            break;
+          case 'FelipeLamparas':
+            precioConDescuento = precioBase - precioBase * 0.10;
+            break;
+          default:
+            precioConDescuento = precioBase - precioBase * 0.05;
+        }
+        break;
+
+      default:
+        precioConDescuento = precioBase;
+        break;
+  }
+
+  // if => importe final + descuento > 120 => +10% (alert esa cantidad de impuesto) 
+  if (precioConDescuento > 120) {
+    impuestos = precioConDescuento * 0.1;
+    campoPrecioDescuento.value = (precioConDescuento + impuestos).toFixed(2);
+
+    alert(`De IIBB usted pagó ${impuestos}`);
+  }
+  else {
+    campoPrecioDescuento.value = precioConDescuento.toFixed(2);
+  }
 }
