580a7cea1220773b8e35133090ccaef2e009ff75
turavinin <aturavinin@gmail.com>
2021-02-09 19:23:49 -0300

    finalizado tema 5, ejerc. 3

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,11 @@
 /*
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
-function mostrar()
-{
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
-}//FIN DE LA FUNCIÓN
+function mostrar() {
+  var claveIngresada = prompt('ingrese el número clave.');
+
+  while (claveIngresada != 'utn750') {
+    claveIngresada = prompt('Clave incorrecta. Ingrese de nuevo');
+  }
+
+  alert('Ingreso exitoso');
+} //FIN DE LA FUNCIÓN
