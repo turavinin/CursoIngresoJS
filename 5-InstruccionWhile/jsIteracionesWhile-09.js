39b12c2712f67c17d86f8de0463b7b0e416837db
turavinin <aturavinin@gmail.com>
2021-02-09 20:45:28 -0300

    finalizado tema 5, ejerc. 9

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,34 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+function mostrar() {
+  // declarar variables
+  var banderaDelPrimero;
+  var numeroIngresado;
+  var numeroMaximo;
+  var numeroMinimo;
+  var respuesta;
+  //iniciar variables
+  banderaDelPrimero = 'es el primero';
+  respuesta = 'si';
+
+  while (respuesta == 'si') {
+    numeroIngresado = parseInt(prompt('Ingrese un numero'));
+    if (banderaDelPrimero == 'es el primero') {
+      banderaDelPrimero = 'no es el primero';
+      numeroMaximo = numeroIngresado;
+      numeroMinimo = numeroIngresado;
+
+      console.log('primero');
+    } else if (numeroIngresado > numeroMaximo) {
+      numeroMaximo = numeroIngresado;
+    } else if (numeroIngresado < numeroMinimo) {
+      numeroMinimo = numeroIngresado;
+    }
+
+    respuesta = prompt('desea continuar? (si / no)');
+  }
+
+  document.getElementById('txtIdMaximo').value = numeroMaximo;
+  document.getElementById('txtIdMinimo').value = numeroMinimo;
+} //FIN DE LA FUNCIÓN
