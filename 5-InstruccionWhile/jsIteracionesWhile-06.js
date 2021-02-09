0e23a100534e7154c190d8b999d5498542473ea4
turavinin <aturavinin@gmail.com>
2021-02-09 20:20:16 -0300

    finalizado tema 5, ejerc. 6

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,14 @@
-function mostrar()
-{
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+function mostrar() {
+  let contador = 0;
+  let acumulador = 0;
+  let numeroIngresado;
 
-	contador=0;
-	acumulador=0;
-	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+  while (contador < 5) {
+    contador++;
+    numeroIngresado = parseInt(prompt(`Ingrese el ${contador}° numero:`));
+    acumulador += numeroIngresado;
+  }
+
+  document.getElementById('txtIdSuma').value = acumulador;
+  document.getElementById('txtIdPromedio').value = acumulador / 5;
+} //FIN DE LA FUNCIÓN
