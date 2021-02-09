8c50d35e64b61f8357f010091174b961054a9005
turavinin <aturavinin@gmail.com>
2021-02-09 20:38:38 -0300

    finalizado tema 5, ejerc. 8

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,26 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
-function mostrar()
-{
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+function mostrar() {
+  let contador = 0;
+  let respuesta = 'si';
+  let numeroElegido = 0;
+  let multiplicacionNegativos = 1;
+  let sumaPositivos = 0;
 
+  while (respuesta == 'si') {
+    numeroElegido = parseInt(prompt('Ingrese un numero'));
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+    if (numeroElegido >= 0) {
+      sumaPositivos += numeroElegido;
+    } else {
+      multiplicacionNegativos *= numeroElegido;
+    }
 
-}//FIN DE LA FUNCIÓN
+    respuesta = prompt('¿Desea agregar otro numero? (si/no)');
+    contador++;
+  }
+
+  document.getElementById('txtIdSuma').value = sumaPositivos;
+  document.getElementById('txtIdProducto').value = multiplicacionNegativos;
+} //FIN DE LA FUNCIÓN
