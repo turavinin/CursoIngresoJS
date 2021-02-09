af23d0ef731f545696aeb8e771992f745cf864b0
turavinin <aturavinin@gmail.com>
2021-02-09 20:28:31 -0300

    terminado tema 5, ejerc. 7

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -2,17 +2,17 @@
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
-function mostrar()
-{
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+function mostrar() {
+  let contador = 0;
+  let acumulador = 0;
+  let respuesta = 'si';
 
+  while (respuesta == 'si') {
+    acumulador += parseInt(prompt('Ingrese un numero'));
+    respuesta = prompt('¿Desea agregar otro numero? (si/no)');
+    contador++;
+  }
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
-
-}//FIN DE LA FUNCIÓN
+  document.getElementById('txtIdSuma').value = acumulador;
+  document.getElementById('txtIdPromedio').value = acumulador / contador;
+} //FIN DE LA FUNCIÓN
