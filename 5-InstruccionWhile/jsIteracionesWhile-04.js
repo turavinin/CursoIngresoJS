55cf12138fc7cf95ab3cc515dce16037604df7c7
turavinin <aturavinin@gmail.com>
2021-02-09 19:39:39 -0300

    finalizado tema 5, ejerc. 4

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,16 @@
 /*
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
-function mostrar()
-{
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
-}//FIN DE LA FUNCIÓN
+function mostrar() {
+  let numeroIngresado = parseInt(
+    prompt('ingrese un número entre 0 y 9 inclusive.')
+  );
+
+  while (numeroIngresado > 9 || numeroIngresado < 0) {
+    numeroIngresado = parseInt(
+      prompt('Incorrecto. Ingrese un número entre 0 y 9 inclusive.')
+    );
+  }
+
+  document.getElementById('txtIdNumero').value = numeroIngresado;
+} //FIN DE LA FUNCIÓN
