45ab4ed4930db88f7c6fcc357b9e061e2a4badda
turavinin <aturavinin@gmail.com>
2021-02-17 19:00:04 -0300

    finalizado el tp 8

--- 8-TPs/08-jsPiedarPapelTijera2.js
+++ 8-TPs/08-jsPiedarPapelTijera2.js
@@ -1,31 +1,73 @@
 var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+var ContadorDeEmpates = 0;
+var ContadorDeGanadas = 0;
+var ContadorDePerdidas = 0;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 3
-	 	
+function comenzar() {
+  //Genero el número RANDOM entre 1 y 3
+  eleccionMaquina = Math.floor(Math.random() * 3) + 1;
+  console.log(eleccionMaquina);
+} //FIN DE LA FUNCIÓN
 
+function piedra() {
+  if (eleccionMaquina == 1) {
+    alert('Empate!');
+    ContadorDeEmpates++;
+  } else if (eleccionMaquina == 2) {
+    alert('Usted perdió!');
+    ContadorDePerdidas++;
+  } else {
+    alert('Usted gano!');
+    ContadorDeGanadas++;
+  }
 
+  // Mostrar resultado
+  mostrarResultado();
 
-}//FIN DE LA FUNCIÓN
-function piedra()
-{
-	
+  // Nuevo numero random
+  comenzar();
+} //FIN DE LA FUNCIÓN
+function papel() {
+  if (eleccionMaquina == 2) {
+    alert('Empate!');
+    ContadorDeEmpates++;
+  } else if (eleccionMaquina == 3) {
+    alert('Usted perdió!');
+    ContadorDePerdidas++;
+  } else {
+    alert('Usted gano!');
+    ContadorDeGanadas++;
+  }
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
+  // Mostrar resultado
+  mostrarResultado();
 
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
-	
-}//FIN DE LA FUNCIÓN
+  // Nuevo numero random
+  comenzar();
+} //FIN DE LA FUNCIÓN
+function tijera() {
+  if (eleccionMaquina == 3) {
+    alert('Empate!');
+    ContadorDeEmpates++;
+  } else if (eleccionMaquina == 1) {
+    alert('Usted perdió!');
+    ContadorDePerdidas++;
+  } else {
+    alert('Usted gano!');
+    ContadorDeGanadas++;
+  }
 
-function mostrarResultado()
-{
+  // Mostrar resultado
+  mostrarResultado();
 
+  // Nuevo numero random
+  comenzar();
+} //FIN DE LA FUNCIÓN
+
+function mostrarResultado() {
+  document.getElementById('txtIdGanadas').value = ContadorDeGanadas;
+
+  document.getElementById('txtIdPerdidas').value = ContadorDePerdidas;
+
+  document.getElementById('txtIdEmpatadas').value = ContadorDeEmpates;
 }
