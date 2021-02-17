70c6970bcdc32bcc10fdf7549d4a53e1d84edab0
turavinin <aturavinin@gmail.com>
2021-02-17 19:20:46 -0300

    finalizado el tp 9

--- 8-TPs/09-jsAgilidadAritmetica1.js
+++ 8-TPs/09-jsAgilidadAritmetica1.js
@@ -6,15 +6,45 @@ y una de las cuatro operaciones básicas (suma, resta,
  el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
-var respuesta;
-function comenzar()
-{
 
-	
+let numeroRandomUno;
+let numeroRandomDos;
+let numeroParaOperadorRandom;
 
-}//FIN DE LA FUNCIÓN
-function Responder()
-{
-	
+function comenzar() {
+  numeroRandomUno = Math.floor(Math.random() * 10) + 1;
+  numeroRandomDos = Math.floor(Math.random() * 10) + 1;
+  numeroParaOperadorRandom = Math.floor(Math.random() * 4) + 1;
 
-}//FIN DE LA FUNCIÓN
+  console.log(`Numero uno: ${numeroRandomUno}`);
+  console.log(`Numero dos: ${numeroRandomDos}`);
+  console.log(`Numero operador: ${numeroParaOperadorRandom}`);
+} //FIN DE LA FUNCIÓN
+
+function Responder() {
+  let numeroSuma = 1;
+  let numeroResta = 2;
+  let numeroDivision = 3;
+  let numeroMultiplicacion = 4;
+  let resultado;
+  // SUMA
+  if (numeroParaOperadorRandom == numeroSuma) {
+    document.getElementById('txtIdOperador').value = 'Suma';
+    resultado = numeroRandomUno + numeroRandomDos;
+  } else if (numeroParaOperadorRandom == numeroResta) {
+    document.getElementById('txtIdOperador').value = 'Resta';
+    resultado = numeroRandomUno - numeroRandomDos;
+  } else if (numeroParaOperadorRandom == numeroDivision) {
+    document.getElementById('txtIdOperador').value = 'División';
+    resultado = numeroRandomUno / numeroRandomDos;
+  } else {
+    document.getElementById('txtIdOperador').value = 'Multiplicación';
+    resultado = numeroRandomUno * numeroRandomDos;
+  }
+
+  document.getElementById('txtIdPrimerNumero').value = numeroRandomUno;
+  document.getElementById('txtIdSegundoNumero').value = numeroRandomDos;
+  document.getElementById('txtIdRespuesta').value = resultado;
+
+  comenzar();
+} //FIN DE LA FUNCIÓN
